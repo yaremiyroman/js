40fae8117de8cd05bd9625f88742837d96617a5c
andrdrx <yaremiyroman@gmail.com>
2025-08-27 08:09:50 +0300

    webpack 1

---
+++ 34/src/index.js
@@ -0,0 +1,12 @@
+import './styles.css';
+
+import parrots from './image/parrot.jpeg';
+
+const img = document.createElement('img');
+img.src = parrots;
+document.body.appendChild(img);
+
+console.log('I am Entry 3Point 123 >>> ');
+
+
+
